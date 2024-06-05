4c5ad20c9d0947606bdf29b81983ece735946315
Carlos-Cordero-Dev <carloscorderobernabeu@gmail.com>
2024-06-05 12:10:38 +0200

    Day 6 DirectX intitialization

---
+++ DirectX/DirectX11_test/Inputclass.h
@@ -0,0 +1,24 @@
+// Filename: inputclass.h
+////////////////////////////////////////////////////////////////////////////////
+#ifndef _INPUTCLASS_H_
+#define _INPUTCLASS_H_ 1
+
+class InputClass
+{
+public:
+	InputClass();
+	InputClass(const InputClass&);
+	~InputClass();
+
+	void Initialize();
+
+	void KeyDown(unsigned int);
+	void KeyUp(unsigned int);
+
+	bool IsKeyDown(unsigned int);
+
+private:
+	bool m_keys[256];
+};
+
+#endif //_INPUTCLASS_H_
---
+++ DirectX/DirectX11_test/Systemclass.h
@@ -0,0 +1,46 @@
+////////////////////////////////////////////////////////////////////////////////
+// Filename: systemclass.h
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef _SYSTEMCLASS_H_
+#define _SYSTEMCLASS_H_ 1
+
+#define WIN32_LEAN_AND_MEAN 1
+
+#include <windows.h>
+
+#include "Inputclass.h"
+#include "Applicationclass.h"
+
+class SystemClass
+{
+public:
+	SystemClass();
+	SystemClass(const SystemClass&);
+	~SystemClass();
+
+	bool Initialize();
+	void Shutdown();
+	void Run();
+
+	LRESULT CALLBACK MessageHandler(HWND, UINT, WPARAM, LPARAM);
+
+private:
+	bool Frame();
+	void InitializeWindows(int&, int&);
+	void ShutdownWindows();
+
+private:
+	LPCWSTR m_applicationName;
+	HINSTANCE m_hinstance;
+	HWND m_hwnd;
+
+	InputClass* m_Input;
+	ApplicationClass* m_Application;
+};
+
+static LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+static SystemClass* ApplicationHandle = 0;
+
+
+#endif //_SYSTEMCLASS_H_
